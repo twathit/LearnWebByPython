be14daea2fd51883556a1832e844f00e7b694621
twathit <2425092591@qq.com>
2017-08-17 16:22:51 +0800

    app change

--- app.py
+++ app.py
@@ -76,6 +76,18 @@ async def response_factory(app,handler):
         resp.content_type='text/plain;charset=utf-8'
         return resp
     return response
+def datetime_filter(t):
+    delta=int(time.time() - t)
+    if delta<60:
+        return u'1分钟前'
+    if delta<3600:
+        return u'%s分钟前'%（delta//60)
+    if delta<86400:
+        return u'%s小时前'%（delta//3600)
+    if delta<604800:
+        return u'%s天前'%（delta//86400)
+    dt=datetime.fromtimestamp(t)
+    return u'%s年%s月%s日'%(dt.year,dt.month,dt.day)
 async def init(loop):
     await orm.create_pool(loop=loop,**configs.db)
     app=web.Application(loop=loop,middlewares=[logger_factory,auth_factory,response_factory])
